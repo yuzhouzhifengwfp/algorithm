165bb993a12bcbe978af0a4d72aa9590e70d7419
wfp <1828085274@qq.com>
2020-12-29 20:32:36 +0800

    add 10

--- src/main/java/com/wfp/java/algorithm1/Solution.java
+++ src/main/java/com/wfp/java/algorithm1/Solution.java
@@ -1,9 +1,10 @@
 package com.wfp.java.algorithm1;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.junit.Test;
+import sun.text.normalizer.UCharacter;
 
 /**
  * @author wfp
@@ -164,5 +165,234 @@ public class Solution {
                 map.put(key, value);
             }
         }
+        
+        
+        
+        
+        //6
+        public String longestPalindrome(String s) {
+            String result = "";
+            int slength = s.length();
+            for (int i = 0; i < slength; i++) {
+                String odd = extractPalindrome(s, i, i);
+                String even = extractPalindrome(s, i, i + 1);
 
+                if (odd.length() > result.length()) {
+                    result = odd;
+                }
+                if (even.length() > result.length()) {
+                    result = even;
+                }
+            }
+            return result;
+        }
+
+    private String extractPalindrome(String s, int l, int r) {
+        while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r))
+        {
+            l--;
+            r++;
+        }
+        return s.substring(l+1,r);
+    }
+
+    @Test
+    public void testSubstring(){
+        System.out.println("a".substring(0, 1));
+
+    }
+
+
+
+    //7
+
+    class IsValid {
+        Stack<Character> st=new Stack<Character>();
+
+        Map<Character,Character> map=new  HashMap<Character,Character>();
+        {
+
+            map.put('(',')');
+            map.put('[',']');
+            map.put('{','}');
+        }
+
+        public boolean isValid(String s) {
+
+
+
+            int n=s.length();
+            if(n%2==1) {
+                return false;
+            }
+
+            for (int i = 0; i < n; i++) {
+                if(!isValid(s.charAt(i))){
+                    return false;
+                }
+            }
+
+            return st.size()==0;
+
+
+        }
+        private Boolean isValid( char c){
+
+
+            switch (c){
+                case '(':
+                case '[':
+                case '{':
+                    st.push(c);
+
+                    return true;
+                case ')':
+                case ']':
+                case '}':
+                    if(st.size()<=0)
+                    {
+                        return false;
+                    }
+                    Character charprev=st.pop();
+                    if(map.get(charprev)!=c)
+                    {
+                        return false;
+                    }
+                    return true;
+
+            }
+
+            return false;
+        }
+    }
+
+
+    //8
+    class FindKthLargest {
+        public int findKthLargest(int[] nums, int k) {
+shuffle(nums);
+            k = nums.length - k;
+            int lo = 0;
+            int hi = nums.length-1;
+
+            while (lo < hi) {
+                int t = partition(nums, lo, hi);
+                if (t > k) {
+                    hi = t - 1;
+                } else if (t < k) {
+                    lo = t + 1;
+                } else {
+                    break;
+                }
+            }
+            return nums[k];
+        }
+
+        private int partition(int[] nums, int lo, int hi) {
+            int i=lo;
+            int v=nums[hi];
+            for (int j = i; j <hi ; j++) {
+                if(nums[j]<v){
+                    exch(nums,i,j);
+                    i++;
+                }
+            }
+            exch(nums,i,hi);
+            return i;
+        }
+
+        private void exch(int[] nums, int i, int j) {
+            int t=nums[i];
+            nums[i]=nums[j];
+            nums[j]=t;
+        }
+
+        private boolean less(int v, int w) {
+            return v<w;
+        }
+
+        private void shuffle(int a[]) {
+
+            final Random random = new Random();
+            for(int ind = 1; ind < a.length; ind++) {
+                final int r = random.nextInt(ind + 1);
+                exch(a, ind, r);
+            }
+        }
+    }
+
+    //9
+    class TrieNode {
+        public boolean isWord;
+        public TrieNode[] children = new TrieNode[26];
+        public TrieNode() {}
+    }
+
+    public class Trie {
+        private TrieNode root;
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        public void insert(String word) {
+            TrieNode ws = root;
+            for(int i = 0; i < word.length(); i++){
+                char c = word.charAt(i);
+                if(ws.children[c - 'a'] == null){
+                    ws.children[c - 'a'] = new TrieNode();
+                }
+                ws = ws.children[c - 'a'];
+            }
+            ws.isWord = true;
+        }
+
+        public boolean search(String word) {
+            TrieNode ws = root;
+            for(int i = 0; i < word.length(); i++){
+                char c = word.charAt(i);
+                if(ws.children[c - 'a'] == null) return false;
+                ws = ws.children[c - 'a'];
+            }
+            return ws.isWord;
+        }
+
+        public boolean startsWith(String prefix) {
+            TrieNode ws = root;
+            for(int i = 0; i < prefix.length(); i++){
+                char c = prefix.charAt(i);
+                if(ws.children[c - 'a'] == null) return false;
+                ws = ws.children[c - 'a'];
+            }
+            return true;
+        }
+    }
+
+
+    //10
+     class MinDistance {
+        public int minDistance(String word1, String word2) {
+            int m = word1.length();
+            int n = word2.length();
+
+            int[][] cost = new int[m + 1][n + 1];
+            for(int i = 0; i <= m; i++)
+                cost[i][0] = i;
+            for(int i = 1; i <= n; i++)
+                cost[0][i] = i;
+
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(word1.charAt(i) == word2.charAt(j))
+                        cost[i + 1][j + 1] = cost[i][j];
+                    else {
+                        int replace = cost[i][j];
+                        int delete = cost[i][j + 1];
+                        int insert = cost[i + 1][j];
+                        cost[i + 1][j + 1] = Math.min(replace, Math.min(delete, insert)) + 1;
+                    }
+                }
+            }
+            return cost[m][n];
+        }
+    }
 }
